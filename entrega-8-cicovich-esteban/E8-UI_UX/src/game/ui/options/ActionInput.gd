40248c11ee3ce33c4263ed3699fed7fb43b98680
eacico <estebancicovich@gmail.com>
2023-10-19 09:44:11 -0300

    Entrega 8 - bonus: cntroles remapeables

--- entrega-8-cicovich-esteban/E8-UI_UX/src/game/ui/options/ActionInput.gd
+++ entrega-8-cicovich-esteban/E8-UI_UX/src/game/ui/options/ActionInput.gd
@@ -4,19 +4,38 @@ extends Node
 onready var input = $"%Input"
 onready var action = $"%Action"
 
-export (String) var action_input: String setget _set_action_input
+export (String) var action_id: String
 export (String) var action_name: String setget _set_action_name
 
 func _ready() -> void:
-	input.text = action_input
+	set_process_input(false)
 	action.text = action_name
+	if !Engine.editor_hint && InputMap.has_action(action_id):
+		var event: InputEvent = InputMap.get_action_list(action_id)[0]
+		_set_event(event)
 
-func _set_action_input(inp: String) -> void:
-	action_input = inp
-	if Engine.editor_hint && has_node("%Input"):
-		$"%Input".text = inp
+func _input(event: InputEvent) -> void:
+	if !event is InputEventMouseMotion:
+		InputMap.action_erase_events(action_id)
+		InputMap.action_add_event(action_id, event)
+		_set_event(event)
+		set_process_input(false)
+		yield(get_tree().create_timer(0.1), "timeout")
+		input.grab_focus()
+
+func _set_event(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		input.text = event.as_text().get_slice(":", 1).get_slice(",", 0).get_slice("=", 1)
+	else:
+		input.text = event.as_text()
 
 func _set_action_name(nm: String) -> void:
 	action_name = nm
 	if Engine.editor_hint && has_node("%Action"):
 		$"%Action".text = nm	
+
+
+func _on_Input_pressed():
+	set_process_input(true)
+	input.text = "..."
+	input.release_focus()
