ea4c524bd55bca0bb432cfdaf113d4859e985cb1
eacico <estebancicovich@gmail.com>
2023-12-06 11:03:40 -0300

    Engrega 9 sin bonus

---
+++ entrega-9-cicovich-esteban/E9-Sonido/src/game/entities/player/states/JumpState.gd
@@ -0,0 +1,55 @@
+extends AbstractState
+
+onready var jump_sfx:AudioStream = load("res://assets/sound/sfx/jump/12_human_jump_1.wav")
+
+export (int) var jumps_limit:int = 1
+
+var jumps:int = 0
+
+
+func enter() -> void:
+	character.snap_vector = Vector2.ZERO
+	character.velocity.y = -character.jump_speed
+	character._play_animation("jump")
+	character.play_sound(jump_sfx)
+
+
+func exit() -> void:
+	jumps = 0
+
+
+func handle_input(event:InputEvent) -> void:
+	if event.is_action_pressed("dash") && character.move_direction != 0:
+		emit_signal("finished", "dash")
+	elif event.is_action_pressed("jump") && jumps < jumps_limit:
+		jumps += 1
+		character.velocity.y = -character.jump_speed
+		character._play_animation("jump")
+		character.play_sound(jump_sfx)
+
+
+func update(delta: float) -> void:
+	character._handle_weapon_actions(delta)
+	character._handle_move_input()
+	if character.move_direction == 0:
+		character._handle_deacceleration(delta)
+	character._apply_movement()
+	if character.is_on_floor():
+		if character.move_direction == 0:
+			emit_signal("finished", "idle")
+		else:
+			emit_signal("finished", "walk")
+	else:
+		if character.velocity.y > 0:
+			character._play_animation("fall")
+		else:
+			character._play_animation("jump")
+
+
+func handle_event(event: String, value = null) -> void:
+	match event:
+		"hit", "healed":
+			character.sum_hp(value)
+		"hp_changed":
+			if value[0] == 0:
+				emit_signal("finished", "dead")
---
+++ entrega-9-cicovich-esteban/README.md
@@ -0,0 +1,15 @@
+# Entregas 9 - Sonido
+
+Motor: Godot v3.5.2
+
+Descripcion:
+- Agregarle sonido a las acciones del personaje (movimiento, daño, muerte, armas, etc).
+- Agregarle sonido a las acciones de los enemigos.
+- Agregar música de fondo al nivel.
+
+Bonus:
+- Agregar foley (sonido ambiente).
+- Volver dinámica la música de fondo del nivel (reactiva a eventos, por ejemplo).
+- Agregar sonidos de UI.
+- Agregar opciones de configuración de sonido al menú de Opciones (cambiar volumen/silenciar buses).
+- Meter efectos extra a los buses de audio para realzar los sonidos
